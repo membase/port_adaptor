21e9f167d2c0b86ad32cc91f004a767481afb824
Oleg <oleg.baulo@gmail.com>
2010-07-30 23:18:34 +0300

    Fixed bug with not reacting when no output on child process
    Change-Id: Ie5bcc22a1d8223f95297d95271b60a16c936884a
    Reviewed-on: http://review.northscale.com/1433
    Reviewed-by: Aliaksey Kandratsenka <alkondratenko@gmail.com>
    Tested-by: Aliaksey Kandratsenka <alkondratenko@gmail.com>

--- src/windows_adaptor.c
+++ src/windows_adaptor.c
@@ -1,40 +1,43 @@
 /* -*- Mode: C; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*- */
-#include <windows.h> 
+#include <windows.h>
 #include <tchar.h>
 #include <conio.h>
 #include <stdio.h>
 #include <io.h>
 
-#define BUFSIZE 4096 
+const int MILLISECONDS_IN_SECOND = 1000;
 
-// struct to hold congig information.
+// Struct to hold config information.
 typedef struct ConfigType
 {
-    // The time to wait for process after it is asked to terminate.
-    int process_wait_timeout_;
+    // The time to wait for process after it is asked to terminate in milliseconds.
+    int process_wait_timeout_msec_;
     // The command line to start process.
     CHAR *process_cmd_line_;
 } Config;
 
-static HANDLE g_child_std_in_rd = NULL;
-static HANDLE g_child_std_in_wr = NULL;
-static HANDLE g_child_std_out_rd = NULL;
-static HANDLE g_child_std_out_wr = NULL;
+static HANDLE g_child_std_in_rd;
+static HANDLE g_child_std_in_wr;
+static HANDLE g_child_std_out_rd;
+static HANDLE g_child_std_out_wr;
 
 static Config g_config;
-static PROCESS_INFORMATION* g_pchild_proc_info = NULL;
-static FILE* g_log_file;
+static PROCESS_INFORMATION* g_pchild_proc_info;
+static HANDLE g_hchild_tracking_thread;
+static CRITICAL_SECTION g_tear_down_crit;
 
 static int ParseParameters(int argc, CHAR* argv[], Config* pconfig);
 static BOOL CreateChildProcess(Config* pconfig);
-static void TearDownChildProcess(PROCESS_INFORMATION* pproc_info, Config* pconfig);
-static void TrackChildProcess(PROCESS_INFORMATION* pchild_proc_info, Config* pconfig);
+static void TearDownChildProcess(Config* pconfig);
+static void TrackChildProcess(Config* pconfig);
 static BOOL WINAPI OnConsoleCtrlEvent(DWORD ctrl_type);
-static void ReleaseAllOnExit(void);
+static DWORD WINAPI WaitUntillChildProcessExits(LPVOID param);
 static void ErrorExit(PTSTR);
 
 int _tmain(int argc, CHAR *argv[]) {
-    SECURITY_ATTRIBUTES saAttr; 
+    SECURITY_ATTRIBUTES saAttr;
+
+    InitializeCriticalSection(&g_tear_down_crit);
 
     if (!SetConsoleCtrlHandler(OnConsoleCtrlEvent, TRUE)) {
         fprintf(stderr, "Parent: Failed to subscribe to console ctrl events\n");
@@ -46,41 +49,42 @@ int _tmain(int argc, CHAR *argv[]) {
         return -1;
     }
 
-    // Set the bInheritHandle flag so pipe handles are inherited. 
-    saAttr.nLength = sizeof(SECURITY_ATTRIBUTES); 
-    saAttr.bInheritHandle = TRUE; 
-    saAttr.lpSecurityDescriptor = NULL; 
+    // Set the bInheritHandle flag so pipe handles are inherited.
+    saAttr.nLength = sizeof(SECURITY_ATTRIBUTES);
+    saAttr.bInheritHandle = TRUE;
+    saAttr.lpSecurityDescriptor = NULL;
 
-    // Create a pipe for the child process's STDOUT. 
+    // Create a pipe for the child process's STDOUT.
     if (!CreatePipe(&g_child_std_out_rd, &g_child_std_out_wr, &saAttr, 0)) {
-        ErrorExit(TEXT("StdoutRd CreatePipe")); 
+        ErrorExit(TEXT("StdoutRd CreatePipe"));
     }
 
     // Ensure the read handle to the pipe for STDOUT is not inherited.
     if (!SetHandleInformation(g_child_std_out_rd, HANDLE_FLAG_INHERIT, 0)) {
-        ErrorExit(TEXT("Stdout SetHandleInformation")); 
+        ErrorExit(TEXT("Stdout SetHandleInformation"));
     }
 
-    // Create a pipe for the child process's STDIN. 
+    // Create a pipe for the child process's STDIN.
     if (!CreatePipe(&g_child_std_in_rd, &g_child_std_in_wr, &saAttr, 0)) {
-        ErrorExit(TEXT("Stdin CreatePipe")); 
+        ErrorExit(TEXT("Stdin CreatePipe"));
     }
 
-    // Ensure the write handle to the pipe for STDIN is not inherited. 
+    // Ensure the write handle to the pipe for STDIN is not inherited.
     if (!SetHandleInformation(g_child_std_in_wr, HANDLE_FLAG_INHERIT, 0)) {
         ErrorExit(TEXT("Stdin SetHandleInformation"));
     }
 
-    // Create the child process. 
+    // Create the child process.
     if (CreateChildProcess(&g_config)) {
-        TrackChildProcess(g_pchild_proc_info, &g_config);
-        ReleaseAllOnExit();
+        TrackChildProcess(&g_config);
     }
-    return 0; 
-} 
+    return 0;
+}
 
 // Parse command line parameters and store them in given config.
 int ParseParameters(int argc, CHAR* argv[], Config* pconfig) {
+    size_t currsize;
+    int i;
     if (argc < 3) {
         return -1;
     }
@@ -89,11 +93,11 @@ int ParseParameters(int argc, CHAR* argv[], Config* pconfig) {
         return -2;
     }
 
-    pconfig->process_wait_timeout_ = atoi(argv[1]);
+    pconfig->process_wait_timeout_msec_ = atoi(argv[1]) * MILLISECONDS_IN_SECOND;
     pconfig->process_cmd_line_ = strdup(argv[2]);
-    size_t currsize = strlen(pconfig->process_cmd_line_) + 1;
+    currsize = strlen(pconfig->process_cmd_line_) + 1;
 
-    for (int i = 3; i < argc; ++i) {
+    for (i = 3; i < argc; ++i) {
         size_t newsize = currsize + strlen(argv[i]) + 1;
         pconfig->process_cmd_line_ = realloc(pconfig->process_cmd_line_,
                                              newsize);
@@ -112,38 +116,38 @@ int ParseParameters(int argc, CHAR* argv[], Config* pconfig) {
 }
 
 // Create a child process that uses the previously created pipes for STDIN and STDOUT.
-BOOL CreateChildProcess(Config* pconfig) { 
+BOOL CreateChildProcess(Config* pconfig) {
     PROCESS_INFORMATION* pproc_info;
     STARTUPINFOA start_info;
-    BOOL success = FALSE; 
+    BOOL success = FALSE;
+    HANDLE parent_std_out = GetStdHandle(STD_OUTPUT_HANDLE);
 
-    // Set up members of the PROCESS_INFORMATION structure. 
-    pproc_info = malloc(sizeof(PROCESS_INFORMATION));
-    ZeroMemory( pproc_info, sizeof(PROCESS_INFORMATION));
+    // Set up members of the PROCESS_INFORMATION structure.
+    pproc_info = calloc(1, sizeof(PROCESS_INFORMATION));
 
-    // Set up members of the _STARTUPINFOW structure. 
+    // Set up members of the _STARTUPINFOW structure.
     // This structure specifies the STDIN and STDOUT handles for redirection.
 
     ZeroMemory( &start_info, sizeof(STARTUPINFOA));
-    start_info.cb = sizeof(STARTUPINFOA); 
+    start_info.cb = sizeof(STARTUPINFOA);
     start_info.hStdError = g_child_std_out_wr;
-    start_info.hStdOutput = g_child_std_out_wr;
+    start_info.hStdOutput = parent_std_out;
     start_info.hStdInput = g_child_std_in_rd;
     start_info.dwFlags |= STARTF_USESTDHANDLES;
 
-    // Create the child process. 
-    success = CreateProcessA(NULL, 
-        pconfig->process_cmd_line_,     // command line 
-        NULL,          // process security attributes 
-        NULL,          // primary thread security attributes 
-        TRUE,          // handles are inherited 
-        CREATE_NEW_PROCESS_GROUP,             // creation flags 
-        NULL,          // use parent's environment 
-        NULL,          // use parent's current directory 
-        &start_info,  // _STARTUPINFOW pointer 
-        pproc_info);  // receives PROCESS_INFORMATION 
+    // Create the child process.
+    success = CreateProcessA(NULL,
+        pconfig->process_cmd_line_,     // command line
+        NULL,          // process security attributes
+        NULL,          // primary thread security attributes
+        TRUE,          // handles are inherited
+        CREATE_NEW_PROCESS_GROUP,             // creation flags
+        NULL,          // use parent's environment
+        NULL,          // use parent's current directory
+        &start_info,  // _STARTUPINFOW pointer
+        pproc_info);  // receives PROCESS_INFORMATION
 
-    // If an error occurs, exit the application. 
+    // If an error occurs, exit the application.
     if (!success) {
         free(pproc_info);
         g_pchild_proc_info = NULL;
@@ -156,108 +160,91 @@ BOOL CreateChildProcess(Config* pconfig) {
 }
 
 // Asks child process to shutdown. If it is unable to shutdown after some time - kills it.
-void TearDownChildProcess(PROCESS_INFORMATION* pproc_info, Config* pconfig) {
+void TearDownChildProcess(Config* pconfig) {
     DWORD wfso_result;
 
-    if (!GenerateConsoleCtrlEvent(CTRL_BREAK_EVENT, pproc_info->dwProcessId)) {
-        ErrorExit(TEXT("GenerateConsoleCtrlEvent CTRL_BREAK_EVENT"));
-    }
-
-    wfso_result = WaitForSingleObject(pproc_info->hProcess,
-                                      pconfig->process_wait_timeout_);
-
-    if (!wfso_result == WAIT_OBJECT_0) {
-        if(wfso_result == WAIT_TIMEOUT) {
-            fprintf(stderr, "\n->Timeout.\n");
+    EnterCriticalSection(&g_tear_down_crit);
+    if(g_pchild_proc_info) {
+        if (!GenerateConsoleCtrlEvent(CTRL_BREAK_EVENT, g_pchild_proc_info->dwProcessId)) {
+            ErrorExit(TEXT("GenerateConsoleCtrlEvent CTRL_BREAK_EVENT"));
         }
-        TerminateProcess(pproc_info->hProcess, 0);
+
+        wfso_result = WaitForSingleObject(g_pchild_proc_info->hProcess,
+                                          pconfig->process_wait_timeout_msec_);
+
+        if (!wfso_result == WAIT_OBJECT_0) {
+            if(wfso_result == WAIT_TIMEOUT) {
+                fprintf(stderr, "\n->Timeout.\n");
+            }
+            fprintf(stderr, "Waiting for child process failed. Terminating child process.");
+            if(!TerminateProcess(g_pchild_proc_info->hProcess, 1)) {
+                ErrorExit(TEXT("Unable to terminate child process."));
+            }
+        }
+        WaitForSingleObject(g_hchild_tracking_thread, INFINITE);
     }
+    g_pchild_proc_info = NULL;
+    LeaveCriticalSection(&g_tear_down_crit);
 }
 
-// Read output from the child process's pipe for STDOUT
-// and write to the parent process's pipe for STDOUT. 
-// Stop when there is no more data, child process exited or EOF was sent to parent process. 
-void TrackChildProcess(PROCESS_INFORMATION* pchild_proc_info, Config* pconfig) { 
-    DWORD number_of_bytes_read, number_of_bytes_written; 
-    CHAR ch_buf_small[1]; 
-    /* CHAR ch_buf_large[1000];  */
-    BOOL success = FALSE;
-    HANDLE parent_std_out = GetStdHandle(STD_OUTPUT_HANDLE);
-    FILE* conout_file;
-    BOOL should_end = FALSE;
-    //DWORD proc_exit_code;
-    //HANDLE hParentStdIn = GetStdHandle(STD_INPUT_HANDLE);
+// Start a thread to track child process exit and read input.
+// Stop when there is no more data, child process exited or EOF was sent to parent process.
+void TrackChildProcess(Config* pconfig) {
+    DWORD thread_id;
+    int ch;
 
-    // Close the write end of the pipe before reading from the 
+    // Close the write end of the pipe before reading from the
     // read end of the pipe, to control child process execution.
     // The pipe is assumed to have enough buffer space to hold the
     // data the child process has already written to it.
-
     if (!CloseHandle(g_child_std_out_wr)) {
-        ErrorExit(TEXT("StdOutWr CloseHandle")); 
+        ErrorExit(TEXT("StdOutWr CloseHandle"));
     }
 
-    conout_file = fopen("CONOUT$", "r");
-    
-    for (;;) {
-        if(_kbhit()) {
-            int r = getc(conout_file);
-            if(r == EOF) {
-                should_end = TRUE;
-                break;
-            }
-        }
+    // Create a thread
+    g_hchild_tracking_thread = CreateThread(
+                 NULL,         // default security attributes
+                 0,            // default stack size
+                 (LPTHREAD_START_ROUTINE) WaitUntillChildProcessExits,
+                 NULL,         // no thread function arguments
+                 0,            // default creation flags
+                 &thread_id); // receive thread identifier
 
-        success = ReadFile( g_child_std_out_rd, ch_buf_small, 1, &number_of_bytes_read, NULL);
-        if(!success || number_of_bytes_read == 0) {
-            break;
-        }
-
-        success = WriteFile(parent_std_out, ch_buf_small, 
-        number_of_bytes_read, &number_of_bytes_written, NULL);
-
-        _flushall();
-
-        if (! success) break;
-    } 
-
-    if (conout_file) {
-        fclose(conout_file);
+    if(g_hchild_tracking_thread == NULL) {
+        ErrorExit(TEXT("CreateThread error"));
     }
 
-    if (should_end) {
-        TearDownChildProcess(pchild_proc_info, pconfig);
-    }
-} 
+    do {
+        ch = getc(stdin);
+    } while (ch != EOF && ch != '\n');
 
-// Fires when our procss s asked to close.
+    TearDownChildProcess(pconfig);
+}
+
+// Fires when our process is asked to close.
 BOOL WINAPI OnConsoleCtrlEvent(DWORD ctrl_type) {
     (void)ctrl_type;
-
-    TearDownChildProcess(g_pchild_proc_info, &g_config);
-    ReleaseAllOnExit();
-
+    TearDownChildProcess(&g_config);
     return FALSE;
 }
 
-// Free allocated resources.
-void ReleaseAllOnExit(void) {
-    if(g_pchild_proc_info) {
-        free(g_pchild_proc_info);
-        g_pchild_proc_info = NULL;
-    }
+// Waits until process exits.
+DWORD WINAPI WaitUntillChildProcessExits(LPVOID param) {
+    DWORD exit_code;
 
-    if(g_log_file) {
-        fclose(g_log_file);
-        g_log_file = NULL;
+    WaitForSingleObject(g_pchild_proc_info->hProcess, INFINITE);
+    if(!GetExitCodeProcess(g_pchild_proc_info->hProcess, &exit_code)) {
+        exit_code = 1;
     }
+    ExitProcess(exit_code);
+    return 1;
 }
 
-// Format a readable error message, display a message box, 
+// Format a readable error message, write to stderr,
 // and exit from the application.
-void ErrorExit(PTSTR lpszFunction) { 
+void ErrorExit(PTSTR lpszFunction) {
     LPVOID msg_buf;
-    DWORD last_error = GetLastError(); 
+    DWORD last_error = GetLastError();
 
     FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER |
                    FORMAT_MESSAGE_FROM_SYSTEM |
@@ -272,5 +259,5 @@ void ErrorExit(PTSTR lpszFunction) {
             (int)last_error, (const char*)msg_buf);
 
     LocalFree(msg_buf);
-    ExitProcess(1);
+    ExitProcess(-1);
 }
